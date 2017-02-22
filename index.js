a2543116a40da88b81e88441527bb7322bcf4241
jaderager <cameronrager@gmail.com>
2017-02-22 15:59:22 -0800

    add chart graphic

--- index.js
+++ index.js
@@ -2,28 +2,34 @@
 
 /* FOCUS GROUP APP */
 
-var randomInclusive = function(minA,maxA) {
+var randomInclusive = function(minA, maxA) {
   var min = Math.ceil(minA);
   var max = Math.floor(maxA);
   return Math.floor(Math.random() * (max + 1 - min) + min);
 };
 
-//first, let's create the image constructor...
-var DeckItem = function(path, id) {
+// first, let's create the image constructor...
+var DeckItem = function(path, id, title) {
   this.isRedundant = false; // Redundancy is whether a card has already been drawn this round, and would display twice concurrently
   this.isRepetitive = false; // Repetetiveness is whether a card has already been drawn the previous round`
   this.viewCount = 0;
   this.clickCount = 0;
   this.path = path;
   this.id = id;
+  this.title = title;
 };
 
 var imageDeck = [
-  new DeckItem('./img/bag.jpg', 0),new DeckItem('./img/banana.jpg', 1),new DeckItem('./img/bathroom.jpg', 2),new DeckItem('./img/boots.jpg', 3),
-  new DeckItem('./img/breakfast.jpg', 4),new DeckItem('./img/bubblegum.jpg', 5),new DeckItem('./img/chair.jpg', 6),new DeckItem('./img/cthulhu.jpg', 7),
-  new DeckItem('./img/dog-duck.jpg', 8),new DeckItem('./img/dragon.jpg', 9),new DeckItem('./img/pen.jpg', 10),new DeckItem('./img/pet-sweep.jpg', 11),
-  new DeckItem('./img/scissors.jpg', 12),new DeckItem('./img/shark.jpg', 13),new DeckItem('./img/sweep.png', 14),new DeckItem('./img/tauntaun.jpg', 15),
-  new DeckItem('./img/unicorn.jpg', 16),new DeckItem('./img/usb.gif', 17),new DeckItem('./img/water-can.jpg', 18),new DeckItem('./img/wine-glass.jpg', 19)
+  new DeckItem('./img/bag.jpg', 0, 'R2D2 Bag'), new DeckItem('./img/banana.jpg', 1, 'Banana Slicer'),
+  new DeckItem('./img/bathroom.jpg', 2, 'Bathroom Tablet Stand'), new DeckItem('./img/boots.jpg', 3, 'Toeless Rain Boots'),
+  new DeckItem('./img/breakfast.jpg', 4, 'Toaster Coffee Pot'), new DeckItem('./img/bubblegum.jpg', 5, 'Meatball Bubblegum'),
+  new DeckItem('./img/chair.jpg', 6, 'Uncomfortable Chair'), new DeckItem('./img/cthulhu.jpg', 7, 'Cthulhu Figure'),
+  new DeckItem('./img/dog-duck.jpg', 8, 'Doggie Duck Bill'), new DeckItem('./img/dragon.jpg', 9, 'Dragon Meat'),
+  new DeckItem('./img/pen.jpg', 10, 'Cutlery Pencaps'), new DeckItem('./img/pet-sweep.jpg', 11, 'Pet Sweeper'),
+  new DeckItem('./img/scissors.jpg', 12, 'Pizza Scissors'), new DeckItem('./img/shark.jpg', 13, 'Shark Sleeping Bag'),
+  new DeckItem('./img/sweep.png', 14, 'Baby Sweeper'), new DeckItem('./img/tauntaun.jpg', 15, 'StarWars Sleeping Bag'),
+  new DeckItem('./img/unicorn.jpg', 16, 'Unicorn Meat'), new DeckItem('./img/usb.gif', 17, 'USB Tentacle'),
+  new DeckItem('./img/water-can.jpg', 18, 'Tragic Watering Can'), new DeckItem('./img/wine-glass.jpg', 19, 'Overstylized Wine Glass')
 ];
 
 var ImageCard = function(handElement) {
@@ -41,14 +47,14 @@ var ImageHand = function() {
   img2El = document.getElementById('img2');
   img3El = document.getElementById('img3');
 
-  this.currentHand = [new ImageCard(img1El), new ImageCard(img2El), new ImageCard(img3El)]; //attach img elements to hand
+  this.currentHand = [new ImageCard(img1El), new ImageCard(img2El), new ImageCard(img3El)]; // attach img elements to hand
   console.log('printing .currentHand', this.currentHand);
 
   this.prevHandRefs = [];
 
   this.finishedRounds = 0;
 
-  this.getRngDeckItem = function() { //returns the randomly picked DeckItem being dealt whilst iterating over hand in .drawCards();
+  this.getRngDeckItem = function() { // returns the randomly picked DeckItem being dealt whilst iterating over hand in .drawCards();
     return imageDeck[this.rngDeckItemIndex];
   };
 
@@ -61,7 +67,7 @@ var ImageHand = function() {
   };
 
   this.reDrawCard = function() {
-    this.rngDeckItemIndex = randomInclusive(0,19);
+    this.rngDeckItemIndex = randomInclusive(0, 19);
   };
 
   /* REDUNDANCY */
@@ -78,7 +84,7 @@ var ImageHand = function() {
 
   this.setPrevHandRepetetiveness = function(bool) {
     for (var index = 0; index < this.currentHand.length; index++) {
-      this.prevHandRefs[index].isRepetitive = bool; //signifies that these DeckItem's have been drawn this round, for use next round
+      this.prevHandRefs[index].isRepetitive = bool; // signifies that these DeckItem's have been drawn this round, for use next round
     }
   };
 
@@ -98,7 +104,7 @@ var ImageHand = function() {
     console.log('.addDrawnCardToHand() :: Drew image ' + this.rngDeckItemIndex + '(' + this.getRngDeckItem().path + ') viewCount: ' + this.getRngDeckItem().viewCount);
   };
 
-  /* BEGINNING ROUND */
+  /* BEGIN ROUND */
   this.drawCards = function() {
     console.log('.drawCards() :: ROUND ', this.finishedRounds + 1);
     this.reDrawCard(); // DRAW the first card, does not accept card into hand
@@ -113,7 +119,7 @@ var ImageHand = function() {
 
       /* first round, first card; */
       /* -redundancy- / -repetetiveness- */
-      for (index = 0; index === 0; index++) {//first round, first card; -redundancy- / -repetetiveness-
+      for (index = 0; index === 0; index++) {// first round, first card; -redundancy- / -repetetiveness-
         console.log('.drawCards() :: first round, first card');
         // console.log('.drawCards() :: ITERATE i = ' + i);
 
@@ -140,12 +146,6 @@ var ImageHand = function() {
 
       console.log('.drawCards() :: n+1 round;');
 
-      this.copyCurrentHandToPrevHand(); // initialize prevHandRefs from currentHand before replacing
-      this.setPrevHandRepetetiveness(true); // first hand redundant on next round
-
-      this.setPrevHandRedundancy(false); // redundancy flags on previous hand must be cleared before each round
-      //... redundancy is set to true per card drawn via .addDrawnCardToHand();
-
       /* n+1 round, first card; */
       /* -redundancy- / +repetetiveness+ */
       for (index = 0; index === 0; index++) { // CHECK REPETITIVENESS
@@ -165,17 +165,17 @@ var ImageHand = function() {
       for (index = 1; index < this.currentHand.length; index++) { // CHECK REDUNDANCY AND REPETITIVENESS
         console.log('.drawCards() :: n+1 round, n+1 card;');
 
-        while ( (this.getRngDeckItem().isRedundant || this.getRngDeckItem().isRepetitive) && runaway < 20) {
+        while ((this.getRngDeckItem().isRedundant || this.getRngDeckItem().isRepetitive) && runaway < 20) {
 
           var debug;
-          if (this.getRngDeckItem().isRedundant){
+          if (this.getRngDeckItem().isRedundant) {
             debug = 'REDUNDANCY: ';
           } else if (this.getRngDeckItem().isRepetitive) {
             debug = 'REPETITIVENESS: ';
           }
 
           console.log('.drawCards() :: ', debug, this.rngDeckItemIndex, '(', this.getRngDeckItem().path, ')');
-          this.reDrawCard(); //REJECT nth card
+          this.reDrawCard(); // REJECT nth card
           runaway++;
         }
         runaway = 0;
@@ -185,14 +185,25 @@ var ImageHand = function() {
     }
 
 
-  };//.drawCards
+  };// .drawCards
 
   /* ENDING ROUND */
-  this.selectHandNo = function(n) { //performs operations on current hand
-    this.currentHand[n].deckItemRef.clickCount++; //increment .clickCount on reference DeckItem's
+  this.selectHandNo = function(n) { // performs operations on current hand
+    this.currentHand[n].deckItemRef.clickCount++; // increment .clickCount on reference DeckItem's
 
-    if (this.finishedRounds > 0) {
-      this.setPrevHandRepetetiveness(false); //clear old repetetiveness flags
+    if (this.finishedRounds === 0) { // first round;
+      this.copyCurrentHandToPrevHand();
+      this.setPrevHandRedundancy(false);
+      this.setPrevHandRepetetiveness(true);
+    }
+
+    if (this.finishedRounds > 0) { // n + 1 round;
+      this.setPrevHandRepetetiveness(false); // clear old repetetiveness flags
+
+      this.copyCurrentHandToPrevHand();
+      this.setPrevHandRedundancy(false); // redundancy flags on previous hand must be cleared before each round
+      // ... redundancy is set to true per card drawn via .addDrawnCardToHand();
+      this.setPrevHandRepetetiveness(true);
     }
 
     /* END ROUND */
@@ -202,10 +213,8 @@ var ImageHand = function() {
       console.log('END ROUND ', this.finishedRounds + 1, ' img', i, ': id=', this.currentHand[i].deckItemRef.id, ' .path=', this.currentHand[i].deckItemRef.path);
     }
 
-    /* BEGIN ROUND */
-    this.drawCards();
   };
-};//ImageHand
+};// ImageHand
 
 var removeImgElAttrs = function(attrKey, imageHand) {
   for (var i = 0; i < imageHand.currentHand.length; i++) {
@@ -220,44 +229,136 @@ var appPane;
 var imageHand;
 document.addEventListener('DOMContentLoaded', function loadWrapper() {
   domLoaded = true;
+});// DOMContentLoaded
 
-});//DOMContentLoaded
-
-introText = document.getElementById('introText');
-introText.addEventListener('click', function introClick() {
-
-  if (windowLoaded && domLoaded) { //Initialize screen
-
-    imageHand.drawCards();
-
-    introText.setAttribute('class', 'hidden');
-    removeImgElAttrs('class', imageHand); //remove 'hidden' class
-    uiState = 'choosing';
-
-
-  }
-});//introClick
-
-imageHand = new ImageHand();
-appPane = document.getElementById('appPane');
-appPane.addEventListener('click', function productClick() {
-  if (uiState === 'choosing') {
-    for (var c = 0; c < imageHand.currentHand.length; c++) {
-      if (event.target === imageHand.currentHand[c].handElement) { //event delegation checks all hand elements
-        imageHand.selectHandNo(c); //select hand index, increment rounds and draw new cards
-      }
-    }
-  }
-});
-
+/* SIGNAL LOADING COMPLETE */
 var loadingText;
 var readyText;
 var windowLoaded = false;
-window.addEventListener('load', function focusGroupApp() {
+window.addEventListener('load', function windowLoadTrigger() {
+  console.log('derp');
   windowLoaded = true;
   loadingText = document.getElementById('loadingText');
   loadingText.setAttribute('class', 'hidden');
 
   readyText = document.getElementById('readyText');
-  readyText.removeAttribute('class'); //remove hidden class, make visible
-});//all content loaded
+  readyText.removeAttribute('class'); // remove hidden class, make visible
+});// all content loaded
+
+document.addEventListener('keypress', function debugMenu(event) {
+  console.log('keyCode:', event.key);
+  if (event.key === 'D') {
+    var uiState = prompt('setUiState() requires arguments');
+    setUiState(uiState);
+  }
+});
+
+var appResults = document.getElementById('appResults');
+var appResultsContext = appResults.getContext('2d');
+console.log('appResults:', appResults);
+function setUiState(uiStateArg) {
+  console.log('setUiState() :: FUNCTION_EXECUTE');
+  uiState = uiStateArg; //set the global
+
+  if (uiState === 'choosing') {
+    for (var c = 0; c < imageHand.currentHand.length; c++) {
+      console.log('clicked target:', event.target);
+
+      if (event.target === imageHand.currentHand[c].handElement) { // event delegation checks all hand elements
+        console.log('supposed to match against:', imageHand.currentHand[c].handElement);
+        imageHand.selectHandNo(c); // select hand index, increment rounds and draw new cards
+
+        if (imageHand.finishedRounds < 25) {
+          imageHand.drawCards();
+        }
+      }
+    }
+  } else if (uiState === 'results') {
+    console.log('uiState=', uiState);
+
+    img1El.setAttribute('class', 'hidden');
+    img2El.setAttribute('class', 'hidden');
+    img3El.setAttribute('class', 'hidden');
+
+    /* fill up the barData object... */
+
+    var barData = new Object();
+    barData.type = 'bar',
+    barData.datasets = [
+      {
+        fillColor: '#FFFFFF',
+        strokeColor: '#FFFFFF',
+      },
+      {
+        fillColor: '#FF0000',
+        strokeColor: '#FF0000'
+      },
+    ];
+
+    appResults.removeAttribute('class'); // remove hidden class, show app results
+
+    var resultsChartLabels = [];
+    var resultsChartClickCount = [];
+    var resultsChartViewCount = [];
+    for (var i = 0; i < imageDeck.length; i++) {
+      resultsChartLabels.push(imageDeck[i].title);
+      resultsChartClickCount.push(imageDeck[i].clickCount);
+      resultsChartViewCount.push(imageDeck[i].viewCount);
+    }
+
+    console.log('resultsChartLabels is:', resultsChartLabels);
+
+    // var resultsChart =
+    new Chart(appResultsContext, {
+      type: 'bar',
+      data: {
+        labels: resultsChartLabels,
+        datasets: [
+          {
+            label: 'clickCount',
+            backgroundColor: '#000000',
+            borderColor: '#000000',
+            borderWidth: 1,
+            data: resultsChartClickCount,
+          },
+          {
+            label: 'viewCount',
+            backgroundColor: '#FF0000',
+            borderColor: '#FF0000',
+            borderWidth: 1,
+            data: resultsChartViewCount,
+          }
+        ], // datasets
+      }, // data object
+    });
+  } // else if
+}//uiState()
+
+
+imageHand = new ImageHand();
+appPane = document.getElementById('appPane');
+
+
+introText = document.getElementById('introText');
+
+appPane.addEventListener('click', function appClickTrigger() {
+
+  document.getSelection().removeAllRanges();
+
+  if (windowLoaded && domLoaded && uiState === 'loading') {
+
+    imageHand.drawCards();
+
+    introText.setAttribute('class', 'hidden');
+    removeImgElAttrs('class', imageHand); // remove 'hidden' class
+    uiState = 'choosing';
+  }
+
+  if (uiState === 'choosing') {
+    setUiState('choosing');
+  }
+
+  if (uiState === 'choosing' && imageHand.finishedRounds === 25) {
+    setUiState('results');
+  }
+});
---
+++ todo.md
@@ -0,0 +1,7 @@
+# todo.md
+
+* Create shift-D key combo to bypass screens .keyCode, keyCtrl
+* Finish exit screens
+* Bar graph results
+* Seriously consider shorter functions and methods in future projects
+ * For loops tend to overcomplicate functions - consider placing them outside in main execution, and making functions operate per-single-operation.
